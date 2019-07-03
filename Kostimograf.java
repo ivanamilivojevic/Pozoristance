b7cf92af75e0179db3e938d7d5fd8668eafd6ac6
Ivana Milivojevic <50844798+ivanamilivojevic@users.noreply.github.com>
2019-07-03 19:59:50 +0200

    Add files via upload

---
+++ Kostimograf.java
@@ -0,0 +1,14 @@
+package pozoriste;
+
+public class Kostimograf extends Zaposleni{
+
+	public Kostimograf(String ime, Pozoriste pozoriste) {
+		super(ime, pozoriste);
+	}
+
+	@Override
+	public String imePosla() {
+		return "Kostimograf";
+	}
+
+}
